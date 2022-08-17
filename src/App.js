931a56b8b8801a215e3dedd9392659ff5039e07b
krishan kant sharma <kk41110002@gmail.com>
2022-08-17 12:57:51 +0530

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
-import React from 'react';
-import './App.css';
-import ToDoList from "./ToDoList.js"
-import { Route, Switch } from "react-router-dom"  
-import Contect from './contect.js';
-import Menu from './Menu';
+import React, { useState } from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@material-ui/icons/Delete";
 
+function App() {
+  let [task, updateTask] = useState();
+  let [arr, updateArr] = useState([]);
+  function change(e) {
+    updateTask(e.target.value);
+  }
 
-
-function App(){
-  
-  return(
+  function mkarr() {
+    updateArr((prev) => {
+      return [...prev, task];
+    });
+    updateTask("");
+  }
+  return (
     <>
-    <Menu/>
-    <Switch>
-
-    <Route exact path="/" component={ToDoList}/>
-    <Route exact  path="/contect/" component={Contect}/>
-    
-    </Switch>
-   
+      <div className="root">
+        <div className="main">
+          <h1>
+            ToDo List <span> 📝</span>
+          </h1>
+          <input
+            type="text"
+            placeholder="write your task"
+            onChange={change}
+            value={task}
+          />
+          <Button className="btn" onClick={mkarr}>
+            <AddIcon className="plus"></AddIcon>
+          </Button>
+          <ul>
+            {arr.map((value, index) => {
+              return <List index={index} value={value} />;
+            })}
+          </ul>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-  
 
+function List(props) {
+  let [line, ch] = useState(false);
+  let [dis, disp] = useState(true);
 
+  let cl = () => {
+    ch(true);
+    disp(false);
+  };
 
-    export default App;
+  return (
+    <>
+      <li
+        key={props.index}
+        style={{ textDecoration: line ? "line-through" : "none" }}
+      >
+        {props.index + 1}. {props.value}{" "}
+        <DeleteIcon
+          className="del"
+          style={{ display: dis ? "inline-block" : "none" }}
+          id={props.index}
+          onClick={cl}
+        />
+      </li>
+    </>
+  );
+}
+
+export default App;
